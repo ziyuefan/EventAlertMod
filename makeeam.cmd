b8b5b5ab05f1692ee7ffb96fa09e9043c98b181a
ziyuefan <43425434+ziyuefan@users.noreply.github.com>
2020-10-26 03:47:39 +0800

    Add files via upload

--- makeeam.cmd
+++ makeeam.cmd
@@ -1,8 +1,9 @@
-for /f "tokens=1-3 delims=/" %%i in ("%DATE%") do set MYDATE=%%i.%%j.%%k
-for /f "tokens=1-2 delims=:" %%i in ("%TIME%") do set MYTIME=%%i%%j
+for /f "tokens=1-3 delims=/ " %%i in ('date/t') do set MYDATE=%%i.%%j.%%k
+for /f "tokens=1-2 delims=:" %%i in ('time/t') do set MYTIME=%%i%%j
 set srcpath=\\NAS\wow_interface\EventAlertMod
 set addonsname=EventAlertMod
 set wowversion=9.0.1
 "C:\Program Files\7-Zip\7z.exe" a "%srcpath%\%addonsname%_%wowversion%_%MYDATE%_%MYTIME%.zip" "C:\Program Files (x86)\World of Warcraft\_ptr_\Interface\AddOns\EventAlertMod\"
 "C:\Program Files\7-Zip\7z.exe" a "%srcpath%\%addonsname%_%wowversion%_%MYDATE%_%MYTIME%.zip" "C:\Program Files (x86)\World of Warcraft\_ptr_\Interface\AddOns\!Lib_ZYF\"
+explorer "%srcpath%\"
 pause
